8addc3a122f041e1ef20bb741162c7f9a59934bd
Kliver Daniel <kliver1998@gmail.com>
2022-02-12 23:57:22 -0500

    Added webpack

---
+++ src/containers/App.svelte
@@ -0,0 +1,13 @@
+<style>
+  :global(body) {
+    background-color: rgb(131, 131, 131);
+    color: white;
+  }
+  h1 {
+    text-align: center;
+  }
+</style>
+
+<div class="main">
+  <h1>Welcome to Instacats webpage</h1>
+</div>
